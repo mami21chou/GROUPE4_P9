0a8b1cbe713408a5b683ae737a01e6dc3e812f86
Aissata-Sow788 <saissata676@gamail.com>
2025-12-12 00:13:26 +0000

    j'ai modifier l'exercice5.py

---
+++ exercice5.py
@@ -0,0 +1,12 @@
+nombre1 = int(input("entrez nombre 1:"))
+nombre2 = int(input("entrez nombre 2:"))
+somme = nombre1 + nombre2
+soustraction = nombre1 - nombre2
+multiplication = nombre1 * nombre2
+division = nombre1 / nombre2
+"""bonjour le monde"""
+
+print(f"la somme de {nombre1} + {nombre2} = {somme}")
+print(f"la soustraction de {nombre1} - {nombre2} = {soustraction}")
+print(f"la multiplication de {nombre1} * {nombre2} = {multiplication}")
+print(f"la division de {nombre1} / {nombre2} = {division}")
